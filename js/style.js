59c701ad0cf05129e761cc68f92ce5268ad9c7bd
badForBoy <731429927@qq.com>
2017-02-05 17:25:45 +0800

    add iconfont and add search

--- js/style.js
+++ js/style.js
@@ -1,7 +1,14 @@
 ;(function(){
+	/*顶部搜索*/
+	var $searchBox = $(".search-item");
+	var $searchItem = $(".search-item").find('span');
+	var $searchMore = $(".search-more");
+	var $searchInput = $(".mod-search__input");
+	/*登录弹窗按钮*/
 	var $loginBtn = $(".modal-login-btn");
 	var $loginAll = $(".modal-login-all");
 	var $loginBg = $(".modal-login-all-bg");
+	var $closeIcon = $("#close-icon");
 
 	var $bg = $(".wrap-banner-bg");
 	var $imgList = $(".modal-big-banner-list > li");
@@ -111,15 +118,46 @@
 
 	});
 
-	/*$loginBtn.on("click",function(){
-		$loginAll.css({"display":"block"})
-	})*/
-
+	/*登录弹窗按钮*/
 	$loginBtn.click(function(){
 		$loginAll.fadeIn(200);
 	});
-	$loginBg.click(function(){
+	$closeIcon.click(function(){
 		$loginAll.fadeOut(200);
 	});
+
+	/*顶部搜索*/
+	$searchItem.hover(function() {
+		//$searchItem.removeClass('hidden');
+		$(this).css({
+			background: '#ddd'
+		});
+	}, function() {
+		$(this).css({
+			background: '#fff'
+		});
+	});
+
+	//搜索栏销量与价格之间的切换
+	$searchItem.click(function(event) {
+			event.preventDefault();
+
+			if ($searchItem.hasClass('hidden')) {
+				$searchItem.removeClass('hidden')
+			} else {
+				$(this).addClass("").siblings($searchItem).addClass("hidden")
+			}
+
+			//$(this).addClass("").siblings($imgBtn).addClass("hidden");
+			if ($searchItem.eq(0).hasClass('hidden')) {
+				$searchInput.attr({
+					placeholder: '按销量搜索'
+				});
+			} else {
+				$searchInput.attr({
+					placeholder: '按价格搜索'
+				});
+			}
+		});
 
 }());
